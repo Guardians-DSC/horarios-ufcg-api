8e87e2242de9cf2f6994f63dfae79212e38d9ab5
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2019-06-04 12:34:18 -0300

    Configuration of eslint and fixes by him

--- src/horarios/controller.js
+++ src/horarios/controller.js
@@ -8,17 +8,17 @@ let horarios;
 loadHorarios().then(response => horarios = response);
 
 router.get('/', (req, res) => {
-  res.status(200).send(horarios);
+    res.status(200).send(horarios);
 });
 
 router.get('/:dia', (req, res) => {
-  const dia = req.params.dia;
+    const dia = req.params.dia;
 
-  if (dia && dias.includes(dia)) {
-    res.status(200).send(horarios.filter(horario => horario.horario.dia === dia));
-  } else {
-    res.status(404).send([]);
-  }
+    if (dia && dias.includes(dia)) {
+        res.status(200).send(horarios.filter(horario => horario.horario.dia === dia));
+    } else {
+        res.status(404).send([]);
+    }
 });
 
 export default router;
