6242f979585e1e5fc1e438204ea12885668aa5d0
Antonio Neto <antoniontv18@gmail.com>
2019-10-06 12:29:23 -0300

    fazendo index.js do util retornar um objeto com funcoes

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,14 +4,14 @@ import fs from 'fs';
  * Lê um arquivo .csv e o retorna como uma promise que possui a string com o conteudo do csv não formatado
  * @param {string} path Caminho para o arquivo .csv
  */
-const readFile = function(path) {
-    var fileContent;
-    return new Promise(function(resolve) {
-        fileContent = fs.readFileSync(path, {encoding: 'utf8'});
-        resolve(fileContent);
-    });
+
+module.exports = {
+    async readFile(path) {
+        var fileContent;
+        return new Promise(function(resolve) {
+            fileContent = fs.readFileSync(path, {encoding: 'utf8'});
+            resolve(fileContent);
+        });
+    }
 };
 
-export {
-    readFile,
-};
