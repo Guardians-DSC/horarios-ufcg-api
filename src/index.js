a39e64ef13d3f7873eecbc2174c0e032c3579fd4
Antonio Neto <antoniontv18@gmail.com>
2019-10-06 22:07:56 -0300

    Adicionando rotina que atualiza o csv de 2 em 2 horas

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { CronJob } from 'cron';
+import updateCsv from './utils/config';
+
+const job = new CronJob('* */2 * * *', () => {
+    updateCsv();
+    console.log('Atualizando a planilha');
+});
+
+job.start();
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
