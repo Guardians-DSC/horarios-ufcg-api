f40d08805af035e4f2d0b5b36980b0febbda4a5a
douglaslimaxx <douglas.lima@ccc.ufcg.edu.br>
2019-11-09 16:08:36 -0300

    add documentation of index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,39 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
+/**
+ * Module Index
+ * @module index
+ */
+
+ /**
+  * Constante que especifica porta usada
+  * @const
+  * @type {number}
+  */
 const PORT = process.env.PORT || 3000;
+
+/**
+ * @const
+ * @type {function}
+ */
 const app = express();
 
+/**
+ * @param  {function} cors(
+ */
 app.use(cors());
+/**
+ * @param  {string} path
+ * @param  {function} routes
+ */
 app.use('/horarios', routes);
 
 /**
  * Confirmação de inicialização do servidor.
+ */
+/**
+ * @param  {const} PORT
+ * @param  {function} console.log('ServidorIniciado')
  */
 app.listen(PORT, () => console.log('Servidor Iniciado'));
