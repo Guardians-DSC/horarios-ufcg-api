37c1faedbf3354b24dae56773b707741165bd8b9
douglaslimaxx <douglas.lima@ccc.ufcg.edu.br>
2019-11-09 15:53:54 -0300

    add documentation of routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,36 @@
 import express from 'express';
 import HorarioController from './controllers/HorarioController';
 
+/**
+ * Module Router
+ * @module router
+ */
+
+ /**
+ * Express router to mount user related functions on.
+ * @type {object}
+ * @const
+ */
 const router = express.Router();
 /**
  * Rota para listagem de todos os horarios
+ * @name get/
+ * @param {string} path  
+ * @param {function}  HorarioController.indexAll
  */
 router.get('/', HorarioController.indexAll);
 /**
  * Rota para listagem dos horarios filtrados pelo dia da semana
+ * @name get/:dia
+ * @param {string} path  
+ * @param {function}  HorarioController.indexByDay
  */
 router.get('/:dia', HorarioController.indexByDay);
 /**
  * Rota para listagem dos horarios filtrados por dia da semana e por hora do dia
+ * @name get/:dia/:hora
+ * @param {string} path  
+ * @param {function}  HorarioController.indexByDayAndHour
  */
 router.get('/:dia/:hora', HorarioController.indexByDayAndHour);
 
