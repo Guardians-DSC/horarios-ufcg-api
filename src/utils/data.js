736b97e51d4de308be88b2895210ba82e4af1663
Jadson Luan <jadsonluan1@gmail.com>
2020-02-20 13:11:57 -0300

    Finaliza implementação do novo leitor de dados

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,6 +1,6 @@
 import { readFile } from '.';
-import nomesDasDisciplinas from './disciplinas.json'
-import subjects from './csv/subjects.json';
+import nomesDasDisciplinas from './disciplinas.json';
+import siglas from './disciplinas-para-siglas.json';
 const CSV_NAME = 'horario20191.csv';
 const data_filename = 'subjects.json';
 
@@ -27,64 +27,61 @@ export function sortBy(criteria, order) {
 const sortByDisciplina = sortBy('disciplina', 1);
 
 const dias = {
-    s: 'segunda',
-    t: 'terca',
-    q: 'quarta',
-    i: 'quinta',
-    x: 'sexta'
+    2: 'segunda',
+    3: 'terca',
+    4: 'quarta',
+    5: 'quinta',
+    6: 'sexta'
 };
 
-const campo_desconhecido = "-";
+const campo_desconhecido = "desconhecido";
+const bloqueado_para_matricula = "(Bloqueada para matrícula)";
 
-const getHoras = data => {
-    let splitted = data.split("-");
-    let inicio = splitted[0];
-    inicio = inicio.split(":")[0];
-    return inicio;
+const getDia = dia => dias[dia];
+const getSala = data => data.substring(1, data.length - 1);
+const getHora = data => {
+    let [horarioInicio, horarioFim] = data.split("-");
+    let [hora, minutos] = horarioInicio.split(":");
+    return hora;
 }
 
-const getSala = data => data.substring(1, -1);
-
 const formatObject = obj => {
-    let { horarios, periodo, turma, codigo, disciplina } = obj;
-    turma = "t" + parseInt(turma);
-    let sala;
+    let { horarios, periodo, turma, disciplina } = obj;
+    let sala = campo_desconhecido;
     horarios = horarios.map(horario => {
-        let parts = horario.split(" ");
-        let dia = dias[parts[0]];
-        let hora = getHoras(parts[1]);
-        sala = getSala(parts[2]);
-        return {
-            horario: {dia,hora}
-        }
+        let [_dia, _hora, _sala] = horario.split(" ");
+        let dia = getDia(_dia);
+        let hora = getHora(_hora);
+        sala = getSala(_sala);
+        return { dia, hora };
     });
-
-    let base = {
-        nome: disciplina,
+    
+    let sigla = siglas[disciplina] || campo_desconhecido;
+    let nome = sigla == campo_desconhecido ? disciplina : nomesDasDisciplinas[sigla];
+    turma = "t" + parseInt(turma);
+    
+    return {
+        nome: sigla,
         sala,
-        disciplina: campo_desconhecido,
+        disciplina: nome,
         turma,
         professor: campo_desconhecido,
         categoria: campo_desconhecido,
         periodo_ppc_novo: periodo,
+        periodo_ppc_antigo: "*",
         sala,
         horarios
     };
-
-    return base;
 }
 
-const duplicaHorarios = dados => {
-    const novosHorarios = [];
+const duplicaDisciplinasPorHorarios = dados => {
+    let disciplinas = [];
     dados.forEach(item => {
-        item.horarios.map(horario => ({
-            ...item,
-            horario
-        }));
+        let { horarios } = item;
         delete item["horarios"];
-        novosHorarios.push(item);
+        horarios.forEach(horario => disciplinas.push({ ...item, horario}) );
     });
-    return novosHorarios;
+    return disciplinas;
 }
 
 /**
@@ -92,8 +89,19 @@ const duplicaHorarios = dados => {
  * Retorna uma promise, contendo os horarios.
  */
 export default async function buildSchedule() {
-    // const response = await fetch("./csv/subjects.json");
-    let horarios = subjects.map(formatObject);
-    horarios = duplicaHorarios(horarios);
-    return horarios.sort(sortByDisciplina);
+    const response = await readFile("src/csv/subjects.json");
+    let disciplinas = JSON.parse(response);
+    
+    // Remove disciplinas que estão bloqueadas para matrícula
+    disciplinas = disciplinas.filter(disciplina => !disciplina.horarios.includes(bloqueado_para_matricula));
+
+    // Remove horarios vazios (isso ocorre pois as disciplinas costumam ter dois horários mas algumas não tem)
+    // Exemplo: didática I
+    disciplinas.forEach(disciplina => {
+        if (disciplina.horarios.includes("")) disciplina.horarios = disciplina.horarios.filter(horario => horario !== "");
+    });
+
+    disciplinas = disciplinas.map(formatObject);
+    disciplinas = duplicaDisciplinasPorHorarios(disciplinas);
+    return disciplinas.sort(sortByDisciplina);
 }
